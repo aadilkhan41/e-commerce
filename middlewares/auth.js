6c331714b9903c1be51d31075d1cd0cb4d7838c0
Aadil <aadilkhan3007@gmail.com>
2025-06-27 13:31:31 +0530

    README.md file update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,15 +5,18 @@ const UserModel = require("../models/user.model");
 dotenv.config();
 
 const authMiddleware = async (req, res, next) => {
+    console.log(0);
     const token = req.headers.authorization?.split(" ")?.[1];
+    console.log(1);
     if (!token) {
         return res.status(401).json({
             success: false,
             message: "Unauthorized",
         });
     }
-
+    console.log(2);
     try {
+        console.log(3);
         const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await UserModel.findById(data.id);
         req.user = user;
